f4996ff50a9432e7aecf34a1a607efd327a41ecf
Monish-06 <70092044+Monish-06@users.noreply.github.com>
2025-04-03 22:39:06 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,3 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def health_check():
-    return "Bot is running!", 200
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
 from pyrogram import Client
 from config import Config
 from handlers import filters_handler, filestore_handler
@@ -26,3 +16,13 @@ filestore_handler.register_handlers(bot)
 if __name__ == "__main__":
     print("Bot is running...")
     bot.run()
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def health_check():
+    return "Bot is running!", 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
